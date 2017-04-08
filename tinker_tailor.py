d0582cfd714dab8bfc80359011841a7b8325ce0a
Mátyás Krista <nincs.matyas@gmail.com>
2017-04-08 19:10:32 +0200

    Modified output

--- tinker_tailor.py
+++ tinker_tailor.py
@@ -1,30 +1,40 @@
-from time import time
+from datetime import datetime
+
 
 class TinkerTailorBasic:
 
     def __init__(self, num, step):
         self.players = []
+        self.result = []
         self.num = num + 1
         self.step = step - 1
         self.pos = self.step
 
     def pick(self):
-        self.players.pop(self.pos)
+        picked = self.players.pop(self.pos)
+        self.result.append(picked)
 
-    def run(self):
+    def setup(self):
         for i in range(1,self.num):
             self.players.append(i)
-        while len(self.players) > 1:
+
+    def loop(self):
+        while len(self.players) > 0:
             self.pick()
-            self.pos = (self.pos + self.step) % len(self.players)
-        self.print()
+            if self.players:
+                self.pos = (self.pos + self.step) % len(self.players)
 
     def print(self):
-        print("The last person is standing on number ", self.players[0], ".")
+        print("The players were picked in the following order: ", self.result)
+
+    def run(self):
+        self.setup()
+        self.loop()
+        self.print()
 
 if __name__ == '__main__':
     n, s = input('Enter the number of people: '), input('Enter the number of steps: ')
     proc = TinkerTailorBasic(int(n), int(s))
-    start = time()
+    start = datetime.now()
     proc.run()
-    print('Time elapsed: ', time()-start, ' seconds.')
+    print('Time elapsed: ', datetime.now()-start, '.')
